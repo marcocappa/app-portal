ea93dd65cb3d4b95eb2be8d04c240d752edbf7b1
Marco Cappa <marcocappa85@gmail.com>
2021-11-22 16:43:17 +0000

    fix currency

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,11 @@ import { format } from "date-fns";
 export const FORMAT_DATE = "do MMMM yyyy";
 
 export const formatDate = (date) => format(new Date(date), FORMAT_DATE);
+
+const formatter = new Intl.NumberFormat("en-GB", {
+  style: "currency",
+  currency: "GBP",
+  minimumFractionDigits: 2,
+});
+
+export const formatCurrency = (n) => formatter.format(n);
