38eef4d7cabb611fab18ae92bac9942b7674e44d
Marco Cappa <marcocappa85@gmail.com>
2021-11-22 16:44:58 +0000

    Add test

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,7 +1,11 @@
-import { formatDate } from "./utils";
+import { formatDate, formatCurrency } from "./utils";
 
 test("formatDate", () => {
   const date = "2021-12-02";
-
   expect(formatDate(date)).toBe("2nd December 2021");
+});
+
+test("formatCurrency", () => {
+  const currency = 1000;
+  expect(formatCurrency(currency)).toBe("£1,000.00");
 });
